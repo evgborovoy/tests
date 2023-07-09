c7e02179d4318d223cd33400935d0924da36489c
Evgeniy Borovoy <borovoyevgen@gmail.com>
2023-07-10 01:05:49 +0400

    some changes

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Stack {
+public class Stack {
+    private int[] stck;
+    private int tos;
+
+    public Stack(int size) {
+        stck = new int[size];
+        tos = -1;
+    }
+
+    void push(int item) {
+        if (tos == stck.length) {
+            System.out.println("стек полон.");
+        } else {
+            stck[++tos] = item;
+        }
+    }
+
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Стек опустошен.");
+            return 0;
+        } else {
+            return stck[--tos];
+        }
+    }
 }
